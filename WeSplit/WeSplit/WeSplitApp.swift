31d0fb2a1f40cf788d4ac7d7fe6c9f1401b08c4e
michaelwdanko <michaeldanko@icloud.com>
2022-06-28 21:51:46 -0400

    Creates the shell project for HWS #1

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Michael Danko on 6/28/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
