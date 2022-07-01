76fe14d830a293b27e66c1b1b06d4c0e810f4583
michaelwdanko <michaeldanko@icloud.com>
2022-07-01 08:41:25 -0400

    Wraps up the WeSplit project with a functional interface

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,22 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tapCount = 0
-    @State private var name = ""
+    @State private var checkAmount: Double = 0.0
+    @State private var numberofPeople = 2
+    @State private var tipPercentage = 18
+    
+    @State private var customTipEnabled = false
+    
+    var amountPerPerson: Double {
+        // calculate the cost per person
+
+        let numberofPeopleDouble = Double(numberofPeople)
+        let tipPercentageDouble = Double(tipPercentage)
+        
+        let checkTotal = checkAmount + (checkAmount * (tipPercentageDouble / i100))
+        
+        return checkTotal / numberofPeopleDouble
+    }
+    
+    let tipPercentages = [0, 10, 15, 20, 25, 30]
     
     var body: some View {
         NavigationView {
             Form {
-                Button("Tap Me: \(tapCount)") { tapCount += 1 }
+                Section {
+                    TextField("Check Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                            .keyboardType(.decimalPad)
+                    /*
+                     Locale is a massive struct built into iOS that is responsible for storing all the user’s region settings – what calendar they use, how they separate thousands digits in numbers, whether they use the metric system, and more. In our case, we’re asking whether the user has a preferred currency code, and if they don’t we’ll fall back to “USD” so at least we have something.
+                     */
+                } header: {
+                    Text("What is the total check amount?")
+                }
                 
-                TextField("Enter your name", text: $name)
+                Section {
+                    Stepper(value: $numberofPeople, in: 1...100) {
+                        Text("\(numberofPeople)")
+                    }
+                    
+                    /*
+                     - There are several ways to create a picker.
+                     - The code below shows an example where the label is a string and another option where the label is a view.
+                     
+                     Picker(selection: $numberofPeople, content: {
+                         ForEach(2..<100) {
+                             Text("\($0) people")
+                         }
+                     }, label: {
+                         Text("Count")
+                     })
+                     
+                     Picker("Number of people", selection: $numberofPeople) {
+                         ForEach(2..<100) {
+                             Text("\($0) people")
+                         }
+                     }
+                     
+                     */
+                } header: {
+                    Text("How many people are there?")
+                }
                 
-                if name != "" {
-                    Text("My name is \(name)")
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                        .pickerStyle(.segmented)
+                } header: {
+                    Text("How much would you like to tip?")
+                }
+                
+                Section {
+                    Text(amountPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                } header: {
+                    Text("Amount per person")
                 }
                 
             }
-            .navigationTitle("We Split App")
+            .navigationTitle("WeSplit")
         }
     }
 }
